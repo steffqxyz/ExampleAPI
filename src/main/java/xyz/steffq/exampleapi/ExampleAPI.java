6d73f0334edab45404d93c027a2269e2651cf104
steffqxyz <153752374+steffqxyz@users.noreply.github.com>
2024-05-09 22:39:55 +0300

    Update ExampleAPI.java

--- src/main/java/xyz/steffq/exampleapi/ExampleAPI.java
+++ src/main/java/xyz/steffq/exampleapi/ExampleAPI.java
@@ -1,11 +1,6 @@
 package xyz.steffq.exampleapi;
 
-import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public final class ExampleAPI extends JavaPlugin {
 
